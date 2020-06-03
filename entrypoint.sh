b2030556d997abdf73b1ee86ef5439346339f32b
Magnus <magnus.longva@bouvet.no>
2020-06-03 15:47:04 +0200

    cleaned up debugging settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 venv/bin/./python3 manage.py makemigrations statistics_api
 venv/bin/./python3 manage.py migrate
-#venv/bin/./python3 manage.py runserver 0.0.0.0:"${STATISTICS_API_PORT}"
-venv/bin/./python3 manage.py runserver 0.0.0.0:3308
-
+venv/bin/./python3 manage.py runserver 0.0.0.0:"${STATISTICS_API_PORT}"

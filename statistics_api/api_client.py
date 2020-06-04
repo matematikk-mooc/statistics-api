bf4c77c6f2161675b8eaee745fc85688bd8f0994
Magnus <magnus.longva@bouvet.no>
2020-06-04 16:48:03 +0200

    added pagination of results from canvas

--- statistics_api/api_client.py
+++ statistics_api/api_client.py
@@ -1,9 +1,10 @@
 import json
-from typing import Tuple
+from typing import Tuple, List, Dict
 
 import requests
 
 from statistics_api.definitions import CANVAS_ACCESS_KEY, CANVAS_API_URL, CANVAS_ACCOUNT_ID, CA_FILE_PATH
+
 
 class ApiClient:
 
@@ -15,24 +16,32 @@ class ApiClient:
 
         self.web_session.verify = CA_FILE_PATH
 
-    def get_group_categories_by_course(self, course_id: int) -> Tuple:
-        web_response = self.web_session.get(f"{CANVAS_API_URL}/courses/{course_id}/group_categories")
-        assert web_response.status_code == 200
-        return tuple(json.loads(web_response.text))
+    def get_group_categories_by_course(self, course_id: int) -> Tuple[Dict]:
+        url = f"{CANVAS_API_URL}/courses/{course_id}/group_categories?per_page=100"
+        return self.paginate_through_url(url)
 
-    def get_groups_by_group_category_id(self, group_category_id: int):
-        web_response = self.web_session.get(f"{CANVAS_API_URL}/group_categories/{group_category_id}/groups",
-                                            params={"include[]": "last_activity_at"})
-        assert web_response.status_code == 200
-        return tuple(json.loads(web_response.text))
+    def get_groups_by_group_category_id(self, group_category_id: int) -> Tuple[Dict]:
+        url = f"{CANVAS_API_URL}/group_categories/{group_category_id}/groups?per_page=100"
+        return self.paginate_through_url(url)
 
-    def get_courses(self):
+    def get_courses(self) -> Tuple[Dict]:
         """
             Fetching all courses to which the configured account is root account of.
         :return:
         """
-        web_response = self.web_session.get(f"{CANVAS_API_URL}/users/{CANVAS_ACCOUNT_ID}/courses",
-                                            params={'include[]': 'total_students'})
+        url = f"{CANVAS_API_URL}/users/{CANVAS_ACCOUNT_ID}/courses?include[]=total_students&per_page=100"
+        return self.paginate_through_url(url)
+
+    def paginate_through_url(self, target_url: str, current_items: List = None) -> Tuple[Dict]:
+        if current_items is None:
+            current_items = []
+
+        web_response = self.web_session.get(target_url)
         assert web_response.status_code == 200
-        courses_dicts = json.loads(web_response.text)
-        return courses_dicts
+        new_items = json.loads(web_response.text)
+        current_items += new_items
+        if web_response.links.get('next'):
+            next_page_url = web_response.links['next'].get('url')
+            return self.paginate_through_url(target_url=next_page_url, current_items=current_items)
+        else:
+            return tuple(current_items)
--- statistics_api/db_client.py
+++ statistics_api/db_client.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Tuple
 
 from statistics_api.models.course import Course
 from statistics_api.models.group import Group
@@ -7,32 +7,23 @@ from statistics_api.models.group_category import GroupCategory
 
 class DatabaseClient:
 
-    def insert_courses(self, courses: List):
+    def insert_courses(self, courses: List[Dict]) -> Tuple[Dict]:
         for course in courses:
             db_course = Course(canvas_id=course['id'], name=course['name'],
                                total_nr_of_students=course['total_students'])
             db_course.save()
             course['db_id'] = db_course.pk
-        return courses
+        return tuple(courses)
 
-    def insert_group_categories(self, group_categories: List):
+    def insert_group_categories(self, group_categories: List[Dict]) -> Tuple[Dict]:
         for group_category in group_categories:
             db_group_category = GroupCategory(canvas_id=group_category['id'], name=group_category['name'],
                                               course_id=group_category['course_id'])
             db_group_category.save()
             group_category['db_id'] = db_group_category.pk
-        return group_categories
+        return tuple(group_categories)
 
-    def insert_groups(self, groups):
-        # group_dicts = [dict(
-        #     canvas_id=group['id'],
-        #     group_category_id=group['group_category_id'],
-        #     name=group['name'],
-        #     description=group['description'],
-        #     members_count=group['members_count']
-        # ) for group in groups]
-
+    def insert_groups(self, groups: Tuple[Dict]) -> None:
         db_groups = [Group(canvas_id=group['id'], group_category_id=group['group_category_id'], name=group['name'],
                            description=group['description'], members_count=group['members_count']) for group in groups]
         Group.objects.bulk_create(db_groups)
-        a = 0

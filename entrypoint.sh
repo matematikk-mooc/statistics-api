98a811cc735d49a5fbb15e3dd09e4114449a1bfa
Magnus <magnus.longva@bouvet.no>
2020-06-04 12:32:51 +0200

    added gunicorn server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-venv/bin/./python3 manage.py makemigrations statistics_api
-venv/bin/./python3 manage.py migrate
-venv/bin/./python3 manage.py runserver 0.0.0.0:"${STATISTICS_API_PORT}"
+source venv/bin/activate
+python manage.py makemigrations statistics_api
+python manage.py migrate
+gunicorn --bind 0.0.0.0:"${STATISTICS_API_PORT}" statistics_api.wsgi:application

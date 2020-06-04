6275ab6459aef07a571612e6966dacfcfc61736f
Magnus <magnus.longva@bouvet.no>
2020-06-04 12:55:59 +0200

    added ALLOWED_HOSTS setting to be passed as env variable. fixed bug where DJANGO_DEBUG env is not resolved properly

--- statistics_api/definitions.py
+++ statistics_api/definitions.py
@@ -30,6 +30,6 @@ Base = declarative_base()
 STRFTIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 DJANGO_SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-DJANGO_DEBUG = os.getenv("DJANGO_DEBUG") if os.getenv("DJANGO_DEBUG") is not None else False
+DJANGO_DEBUG = bool(os.getenv("DJANGO_DEBUG")) if os.getenv("DJANGO_DEBUG") is not None else False
 DJANGO_ALLOWED_HOSTS = [s.strip() for s in os.getenv("DJANGO_ALLOWED_HOSTS").split(',')] if os.getenv(
     "DJANGO_ALLOWED_HOSTS") else []

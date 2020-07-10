9aa09881da45108192728df665216a4f62302b5c
Magnus <magnus.longva@bouvet.no>
2020-07-10 13:47:13 +0200

    added single test on group_category_controller

---
+++ statistics_api/tests/test_group_category_controller.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+
+class Test(TestCase):
+
+    def test_group_category(self):
+        client = APIClient()
+        web_response = client.get(
+            path=f"/api/statistics/groupCategory/248")
+        self.assertEqual(200, web_response.status_code)

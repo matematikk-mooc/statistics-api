14e9b91467e05d5c92c69428def8367981efb8ce
Magnus <magnus.longva@bouvet.no>
2020-09-03 16:20:46 +0200

    rewrote date parsing logic in workflow script

--- .github/health_test_src/do_health_test.py
+++ .github/health_test_src/do_health_test.py
@@ -2,7 +2,6 @@
 import json
 import os
 from datetime import datetime
-from json.decoder import JSONDecodeError
 
 import requests
 
@@ -18,8 +17,12 @@ def get_age_of_data_on_statistics_api(base_url: str, municipality_id, course_id)
     response = requests.get(url)
     json_result = json.loads(response.text)
 
-    date_str = json_result["Result"][0]["date"]
-    date_obj = datetime.fromisoformat(date_str[:-1])
+    timestamp_str = json_result["Result"][0]["date"]
+    date_part, time_part = timestamp_str.split("T")
+    year, month, day = [int(i) for i in date_part.split("-")]
+    hour, minute = [int(i) for i in time_part.split(":")[:2]]
+    seconds = int(time_part.split(":")[-1].split(".")[0])
+    date_obj = datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=seconds)
     time_diff = datetime.now() - date_obj
     return int(time_diff.total_seconds())
 

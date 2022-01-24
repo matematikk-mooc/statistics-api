5be2c852a0d7f27d3b6e072fea3877637e0abf62
Magnus <magnus.longva@bouvet.no>
2022-01-24 19:47:13 +0100

    debugging non-responding kpas api

--- statistics_api/tests/test_settings.py
+++ statistics_api/tests/test_settings.py
@@ -4,7 +4,3 @@ SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SECURE_REFERRER_POLICY = False
-if os.environ.get('TESTS_WITHOUT_MIGRATIONS', False):
-    MIGRATION_MODULES = {
-        app.split('.')[-1]: None for app in INSTALLED_APPS
-    }

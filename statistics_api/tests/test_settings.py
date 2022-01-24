045e1eb25edb23423c90452b8a843b2cf80e289d
Magnus <magnus.longva@bouvet.no>
2022-01-24 19:32:11 +0100

    fixed tests pipeline

--- statistics_api/tests/test_settings.py
+++ statistics_api/tests/test_settings.py
@@ -4,3 +4,7 @@ SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SECURE_REFERRER_POLICY = False
+if os.environ.get('TESTS_WITHOUT_MIGRATIONS', False):
+    MIGRATION_MODULES = {
+        app.split('.')[-1]: None for app in INSTALLED_APPS
+    }

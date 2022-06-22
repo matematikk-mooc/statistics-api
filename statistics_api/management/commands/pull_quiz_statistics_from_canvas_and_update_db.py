40a8e2b7ab584c41071bfec446f195df7a43b3d4
Thea Hvalen Thodesen <theaht@redpill-linpro.com>
2022-06-22 15:16:35 +0200

    quiz quantitative data

---
+++ statistics_api/management/commands/pull_quiz_statistics_from_canvas_and_update_db.py
@@ -0,0 +1,90 @@
+import logging
+import sys
+
+from django.core.management import BaseCommand
+from django.db import transaction
+
+from statistics_api.clients.canvas_api_client import CanvasApiClient
+from statistics_api.definitions import CANVAS_ACCOUNT_ID
+from statistics_api.quizzes.models import QuestionStatistics, QuizStatistics, Answer, AnswerSet, SubmissionStatistics
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+        logger = logging.getLogger()
+        api_client = CanvasApiClient()
+        canvas_account_id: int = CANVAS_ACCOUNT_ID if CANVAS_ACCOUNT_ID else api_client.get_canvas_account_id_of_current_user()
+        courses = api_client.get_courses(canvas_account_id=canvas_account_id)
+        for course in courses:
+            course_id = course['id']
+            quizzes = api_client.get_quizzes_in_course(course_id)
+            for quiz in quizzes:
+                quiz_id = quiz['id']
+                self.fetch_statistics_from_single_quiz(api_client, course_id, quiz['id'])
+        #Temp:
+        #self.fetch_statistics_from_single_quiz(api_client, 502, 2720)
+
+    @transaction.atomic
+    def fetch_statistics_from_single_quiz(self, api_client, course_id, quiz_id):
+        response = api_client.get_quiz_statistics(course_id, quiz_id)
+        if response is None:
+            return
+        quiz_data = response['quiz_statistics'][0]
+        statistics_object, created = QuizStatistics.objects.update_or_create(
+            canvas_course_id=course_id,
+            canvas_quiz_id=quiz_id,
+            defaults={'canvas_id' : quiz_data.get('id')})
+
+        question_data = quiz_data['question_statistics']
+        for question in question_data:
+            question_object, created = QuestionStatistics.objects.update_or_create(
+                canvas_id=question.get('id'),
+                defaults={
+                    'question_type' : question.get('question_type'),
+                    'question_text' : question.get('question_text'),
+                    'responses' : question.get('responses'),
+                    'quiz_statistics' : statistics_object})
+
+            if 'answer_sets' in question:
+                answer_sets = question['answer_sets']
+                self.parse_answer_sets(answer_sets, question_object)
+
+            if 'answers' in question:
+                answers = question['answers']
+                self.parse_answers(answers, question_object)
+
+        submission_object = quiz_data['submission_statistics']
+        SubmissionStatistics.objects.update_or_create(
+            quiz_statistics=statistics_object,
+            defaults={
+                'unique_count' : submission_object.get('unique_count')})
+
+    def parse_answer_sets(self, answer_sets, question_object):
+        for answer_set in answer_sets:
+            answer_set_object, created =  AnswerSet.objects.update_or_create(
+                canvas_id = answer_set.get('id'),
+                defaults={
+                    'text': answer_set.get('text'),
+                    'question_statistics' : question_object})
+            answers = answer_set['answers']
+            self.parse_answers(answers, answer_set_object)
+
+    def parse_answers(self, answers, parent_object):
+        if isinstance(parent_object, AnswerSet):
+            for answer in answers:
+                Answer.objects.update_or_create(
+                    canvas_id=answer.get('id'),
+                    answer_sets=parent_object,
+                    defaults={
+                        'text' : answer.get('text'),
+                        'responses' : answer.get('responses')})
+
+        if isinstance(parent_object, QuestionStatistics):
+            for answer in answers:
+                Answer.objects.update_or_create(
+                    canvas_id=answer.get('id'),
+                    question_statistics=parent_object,
+                    defaults={
+                        'text' : answer.get('text'),
+                        'responses' : answer.get('responses')})
--- statistics_api/quizzes/views.py
+++ statistics_api/quizzes/views.py
@@ -1,33 +1,77 @@
 from rest_framework import serializers
 from rest_framework.response import Response
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
-from statistics_api.clients.canvas_api_client import CanvasApiClient
-from statistics_api.quizzes.models import Answer, QuestionStatistics, QuizStatistics, SubmissionStatistics
+from statistics_api.quizzes.models import Answer, AnswerSet, QuestionStatistics, QuizStatistics, SubmissionStatistics
 
 # Create your views here.
 
-
+@api_view(('GET',))
 def quiz_statistics(self, course_id: int, quiz_id: int):
-    api_client = CanvasApiClient()
-    response = api_client.get_quiz_statistics(course_id, quiz_id)
-    quiz_data = response['quiz_statistics'][0]
-    statistics_object = QuizStatistics.objects.create(canvas_course_id=course_id, canvas_quiz_id=quiz_id, canvas_id=quiz_data['id'])
-    question_data = quiz_data['question_statistics']
-    for question in question_data:
-        question_object = QuestionStatistics.objects.create(canvas_id=question['id'], question_type=question['question_type'], question_text=question['question_text'], responses=question['responses'], quiz_statistics=statistics_object)
-        answer_data = question['answers']
-        for answer in answer_data:
-           Answer.objects.create(canvas_id=answer['id'], text=answer['text'], responses=answer['responses'], question_statistics=question_object)
-    submission_object = quiz_data['submission_statistics']
-    SubmissionStatistics.objects.create(unique_count=submission_object['unique_count'], quiz_statistics=statistics_object)
-    #query = QuizStatistics.objects.all().filter(canvas_quiz_id=quiz_id)
-    #result = QuizStatisticsSerializer(query, many=True)
-    #return Response(result.data)
-    return JsonResponse({'result' : quiz_data})
+    query = QuizStatistics.objects.all().filter(canvas_course_id=course_id, canvas_quiz_id=quiz_id)
+    result = QuizStatisticsSerializer(query, many=True)
+    return Response(result.data)
 
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = (
+            'id',
+            'canvas_id',
+            'text',
+            'responses'
+        )
+        depth = 1
+
+class AnswerSetSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True)
+    class Meta:
+        model = AnswerSet
+        fields = (
+            'id',
+            'canvas_id',
+            'text',
+            'answers'
+        )
+        depth = 1
+
+#class OpenAnswerResponseSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = OpenAnswerResponse
+#        fields = (
+#            'id',
+#            'answer'
+#        )
+#        depth = 1
+
+class QuestionStatisticsSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True, required=False)
+    answer_sets = AnswerSetSerializer(many=True)
+    class Meta:
+        model = QuestionStatistics
+        fields = (
+            'id',
+            'canvas_id',
+            'question_type',
+            'question_text',
+            'responses',
+            'answers',
+            'answer_sets',
+        )
+        depth = 1
+
+class SubmissionStatisticsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubmissionStatistics
+        fields = (
+            'id',
+            'unique_count'
+        )
+        depth = 1
 
 class QuizStatisticsSerializer(serializers.ModelSerializer):
+    question_statistics = QuestionStatisticsSerializer(many=True)
+    submission_statistics = SubmissionStatisticsSerializer(many=True)
     class Meta:
         model = QuizStatistics
         fields = (

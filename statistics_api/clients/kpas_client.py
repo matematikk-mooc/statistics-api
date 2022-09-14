70a3e734259e71b8fcec79b9958868cf7b88ac77
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2022-09-14 12:14:05 +0200

    json() instead of json.loads()

--- statistics_api/clients/kpas_client.py
+++ statistics_api/clients/kpas_client.py
@@ -17,21 +17,21 @@ class KpasClient:
 
     def get_schools_by_municipality_id(self, municipality_id: int) -> Tuple[Dict]:
         web_response = self.web_session.get(f"{KPAS_NSR_API_URL}/communities/{municipality_id}/schools/")
-        response_json = json.loads(web_response.text)
+        response_json = web_response.json()
         if response_json.get("result") is None:
             return None
         return tuple(response_json.get("result"))
 
     def get_schools_by_county_id(self, county_id: int) -> Tuple[Dict]:
         web_response = self.web_session.get(f"{KPAS_NSR_API_URL}/counties/{county_id}/schools/")
-        response_json = json.loads(web_response.text)
+        response_json = web_response.json()
         if response_json.get("result") is None:
             return None
         return tuple(response_json.get("result"))
 
     def get_municipalities_by_county_id(self, county_id):
         web_response = self.web_session.get(f"{KPAS_NSR_API_URL}/counties/{county_id}/communities/")
-        response_json = json.loads(web_response.text)
+        response_json = web_response.json()
         if response_json.get("result") is None:
             return None
         return tuple(response_json.get("result"))
@@ -42,21 +42,21 @@ class KpasClient:
 
     def get_county(self, county_id: int) -> Union[Dict, None]:
         web_response = self.web_session.get(f"{KPAS_NSR_API_URL}/counties/{county_id}")
-        response_json = json.loads(web_response.text)
+        response_json = web_response.json()
         if response_json.get("result") is None:
             return None
         return response_json.get("result")
 
     def get_municipality(self, municipality_id: int) -> Union[Dict, None]:
         web_response = self.web_session.get(f"{KPAS_NSR_API_URL}/communities/{municipality_id}")
-        response_json = json.loads(web_response.text)
+        response_json = web_response.json()
         if response_json.get("result") is None:
             return None
         return response_json.get("result")
 
     def get_all_high_schools(self) -> Tuple[Dict]:
         web_response = self.web_session.get(f"{KPAS_NSR_API_URL}/schools", params={"only_high_schools": True})
-        response_json = json.loads(web_response.text)
+        response_json = web_response.json()
         if response_json.get("result") is None:
             return None
         return tuple(response_json.get("result"))

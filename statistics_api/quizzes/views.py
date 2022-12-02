eb18460e48fa1151ffc2e0805821dd25a18b9486
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2022-12-02 08:56:01 +0100

    KURSP-714: Quiz endpoint groups
    Add possibillity to specify groups in quiz endpoints

--- statistics_api/quizzes/views.py
+++ statistics_api/quizzes/views.py
@@ -1,23 +1,53 @@
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.db.models import Prefetch
 
 from statistics_api.quizzes.models import AnswerUserGroup, Answer, AnswerSet, QuestionStatistics, QuizStatistics, SubmissionStatistics
 
 # Create your views here.
 
 @api_view(('GET',))
-def quiz_statistics(self, course_id: int, quiz_id: int):
-    query = QuizStatistics.objects.all().filter(canvas_course_id=course_id, canvas_quiz_id=quiz_id)
+def quiz_statistics(request, course_id: int, quiz_id: int):
+    groups = request.GET.get('groups')
+    print(groups)
+    if groups:
+        groups = groups.split(',')
+        query = QuizStatistics.objects.filter(
+                canvas_course_id=course_id,
+                canvas_quiz_id=quiz_id
+            ).prefetch_related(
+                Prefetch(
+                    'question_statistics__answers__user_groups',
+                    queryset=AnswerUserGroup.objects.filter(group_id__in=groups)
+                )
+            )
+    else:
+        query = QuizStatistics.objects.filter(
+                canvas_course_id=course_id,
+                canvas_quiz_id=quiz_id
+            )
     result = QuizStatisticsSerializer(query, many=True)
     return Response(result.data)
 
 @api_view(('GET',))
-def course_quizzes_statistics(self, course_id: int):
-    query = QuizStatistics.objects.all().filter(canvas_course_id=course_id)
+def course_quizzes_statistics(request, course_id: int):
+    groups = request.GET.get('groups')
+    print(groups)
+    if groups:
+        groups = groups.split(',')
+        query = QuizStatistics.objects.filter(
+                canvas_course_id=course_id
+            ).prefetch_related(
+                Prefetch(
+                    'question_statistics__answers__user_groups',
+                    queryset=AnswerUserGroup.objects.filter(group_id__in=groups)
+                )
+            )
+    else:
+        query = QuizStatistics.objects.filter(canvas_course_id=course_id)
     result = QuizStatisticsSerializer(query, many=True)
     return Response(result.data)
-
 
 class AnswerUserGroupSerializer(serializers.ModelSerializer):
     class Meta:

9c3c81fa8e208af84e3bcc8bfdf23954236340b5
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2023-05-22 11:37:58 +0200

    KURSP-333: Add command to delete kompetanse dev users from Canvas
    Add maintenance command to delete all kompetanse udir dev users from canvas

---
+++ statistics_api/management/commands/maintenance_delete_dev_users.py
@@ -0,0 +1,23 @@
+import logging
+
+from django.core.management import BaseCommand
+from statistics_api.clients.canvas_api_client import CanvasApiClient
+
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument("account_id", nargs="+", type=int)
+
+    def handle(self, *args, **options):
+        account_id=options["account_id"][0]
+        api_client = CanvasApiClient()
+        account_dev_users = api_client.get_udirdev_account_users(account_id)
+        for user in account_dev_users:
+            if self.check_email(user.get("email")):
+                api_client.delete_user(account_id, user.get("id"))
+                print("Deleted user: ", user.get("id"), ", ", user.get("email"))
+
+    def check_email(self, email):
+        return email.startswith("kompetanse") and email.endswith("@udir.dev")

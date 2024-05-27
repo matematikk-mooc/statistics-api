83b979ece18b2ce19c3f841fc3675187b417101f
AjXUdir <ajanth.uthaya@udir.no>
2024-05-27 13:00:16 +0200

    DIT-113: Remove enrollment percentage categories with direct members and teachers count

--- statistics_api/definitions.py
+++ statistics_api/definitions.py
@@ -75,38 +75,3 @@ MYSQL_VARCHAR_DEFAULT_LENGTH = 255
 MYSQL_CASCADE = "CASCADE"
 Base = declarative_base()
 STRFTIME_FORMAT = "%Y-%m-%d %H:%M:%S"
-
-# Codes denoting various levels of teacher enrollment percentages. E.g., a school with enrollment_percentage_category
-# '0' has exactly 0 % course enrollment among teachers, whereas a school with enrollment_percentage_category '1' has
-# (0-20] % course enrollment among teachers.
-PERCENTAGE_INTERVALS = {
-    0: 0,
-    1: 20,
-    2: 40,
-    3: 60,
-    4: 80,
-    5: 100
-}
-
-CATEGORY_CODES = [k for k in PERCENTAGE_INTERVALS.keys()]
-CATEGORY_CODES.sort()
-
-TOO_FEW_TEACHERS_CODE = 99
-TOO_FEW_TEACHERS_CUTOFF = 5
-
-
-enrollment_percentage_category_codes = {CATEGORY_CODES[0]: f"e = {PERCENTAGE_INTERVALS[CATEGORY_CODES[0]]} %"}
-
-for i in range(len(CATEGORY_CODES)-1):
-    category_code = CATEGORY_CODES[i+1]
-    previous_category_code = CATEGORY_CODES[i]
-    lower_bound = PERCENTAGE_INTERVALS[previous_category_code]
-    upper_bound = PERCENTAGE_INTERVALS[category_code]
-    enrollment_percentage_category_codes.update({category_code: f"{lower_bound} % < e <= {upper_bound} %"})
-
-CATEGORY_CODE_INFORMATION_DICT = {
-    "info":
-        {"description": "Category codes denoting intervals of enrollment percentage. e = course enrollment percentage"}
-}
-
-CATEGORY_CODE_INFORMATION_DICT["info"].update({"category_codes": enrollment_percentage_category_codes})
